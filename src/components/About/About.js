94e057f29d3ee67df83927a1bbbbc64071f5c498
Max Garber <bubblebooy@gmail.com>
2020-08-27 12:49:25 -0500

    testing github action

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ class About extends Component {
         <p>I went to University of California Santa Barbara where I got a Bachelor's degree in Physics</p>
         <p>I am in Chicago while my SO is getting a Material Science PHD at North Western</p>
         <p>While in Chicago I have been teaching myself Machine Learning, Web Development, Unity and 3D Modeling</p>
-        <p>Interests include DnD, Boardgames, 3D printing, VR, Food, Programming Projects, Computer Games, Scuba Diving, Skiing </p>
+        <p>Interests include DnD, Boardgames, 3D printing, VR, Food, Programming Projects, Computer Games, Scuba Diving, and Skiing </p>
 
       </div>
     );
